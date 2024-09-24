a524fbe0a5dcefeab0b3bfde34974f15b7679577
benjahuenchunir <bahuenchunir@gmail.com>
2024-09-24 18:23:56 -0300

    test: test auto deploy

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,9 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
     <div id="HomeContainer">
-        <h1>Home</h1>
-     </div>
+      <h1>Home</h1>
+      <button onClick={() => navigate('/signin')}>Go to Sign In</button>
+      <button onClick={() => navigate('/signup')}>Go to Sign Up</button>
+    </div>
   );
 }
 
