dc98773851de4c2aef328fdcb161bddf28893a41
CrisBriones <crisbrioneslabbe@uc.cl>
2024-09-30 11:48:38 -0300

    Arreglaod problema del sign up button, ahora hay que arreglar el url al que te envia este boton

--- src/index.js
+++ src/index.js
@@ -3,17 +3,29 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import { Auth0ProviderWithNavigate } from "./auth0-provider-with-navigate";
+import { Auth0Provider } from '@auth0/auth0-react';
 import "./styles/styles.css";
 
-const container = document.getElementById("root");
+const container = document.getElementById("root")!;
 const root = createRoot(container);
+const domain = import.meta.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = import.meta.env.REACT_APP_AUTH0_CLIENT_ID;
+const redirectUri = import.meta.env.REACT_APP_AUTH0_CALLBACK_URL;
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Auth0ProviderWithNavigate>
-        <App />
-      </Auth0ProviderWithNavigate>
-    </BrowserRouter>
+    <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        authorizationParams={{
+          redirect_uri: window.location.origin
+        }}
+    >
+      <BrowserRouter>
+        <Auth0ProviderWithNavigate>
+          <App />
+        </Auth0ProviderWithNavigate>
+      </BrowserRouter>
+    </Auth0Provider>
   </React.StrictMode>
 );
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,26 @@ import { createRoot } from 'react-dom/client'
 import { App } from "./App";
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = import.meta.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = import.meta.env.REACT_APP_AUTH0_CLIENT_ID;
+const redirectUri = import.meta.env.REACT_APP_AUTH0_CALLBACK_URL;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        authorizationParams={{
+          redirect_uri: window.location.origin
+        }}
+    >
+
+    
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
   </StrictMode>,
 )
