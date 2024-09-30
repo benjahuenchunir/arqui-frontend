eae5ad0ecd1e011a30ee1aefe995586b837ac16e
CrisBriones <crisbrioneslabbe@uc.cl>
2024-09-30 20:02:24 -0300

    Botón Auth0 funcionando

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.scss';
+import LoginButton from "./components/LoginButton";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from './views/home/Home';
 import SignIn from './views/signin/SignIn';
 import SignUp from './views/signup/SignUp';
@@ -8,20 +12,48 @@ import Wallet from './views/wallet/Wallet';
 import ProcesoDeCompras from './views/listadecompras/ListaDeCompras';
 import Compra from './views/compra/Compra';
 
-function App() {
+
+const App = () => {
+  const { isAuthenticated } = useAuth0();
+
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/wallet" element={<Wallet />} />
-        <Route path="/compra" element={<Compra />} />
-        <Route path="/procesocompra" element={<ProcesoDeCompras />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        {isAuthenticated ? (
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/wallet" element={<Wallet />} />
+            <Route path="/compra" element={<Compra />} />
+            <Route path="/procesocompra" element={<ProcesoDeCompras />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        ) : (
+          <LoginButton />
+        )}
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/signin" element={<SignIn />} />
+//         <Route path="/signup" element={<SignUp />} />
+//         <Route path="/wallet" element={<Wallet />} />
+//         <Route path="/compra" element={<Compra />} />
+//         <Route path="/procesocompra" element={<ProcesoDeCompras />} />
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom';
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
+// import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </StrictMode>,
+// )
+
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+ 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+
+
+root.render(
+  <Auth0Provider
+  domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: redirectUri,
+      }}>
+  <App />
+</Auth0Provider>
+);
