ff36cc1743e01aac7b722b90d4ffbb7d98598b65
benjahuenchunir <bahuenchunir@gmail.com>
2024-10-01 21:00:19 -0300

    fix: use content type json

--- src/axiosConfig.tsx
+++ src/axiosConfig.tsx
@@ -7,5 +7,6 @@ const API_KEY = import.meta.env.VITE_BACKEND_API_KEY as string;
 axios.defaults.baseURL = `${BACKEND_PROTOCOL}://${BACKEND_HOST}`;
 
 axios.defaults.headers.common['X-Api-Key'] = API_KEY;
+axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 export default axios;
