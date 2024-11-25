88c4a4d8730beebbba6acd8a26f498b281672546
bahuenchunir <bahuenchunir@uc.cl>
2024-11-25 16:40:35 -0300

    feat: view available fixtures and post offers

--- src/views/admin/Dashboard.tsx
+++ src/views/admin/Dashboard.tsx
@@ -1,12 +1,176 @@
-import "./Dashboard.scss";
-import { Typography } from "@mui/material";
+import '../../index.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useModal } from '../../components/Modal/ModalContext';
+import './Dashboard.scss';
+import { Fixture, OddValue } from '../../types/backend.ts';
+import { FixtureCard } from './FixtureCard.tsx';
+
+interface Offer {
+  fixture_id: number
+  result: string
+  quantity: number
+  uid: string
+}
 
 function Dashboard() {
+  const { user, loginWithRedirect } = useAuth0();
+  const [fixtures, setFixtures] = useState<Fixture[]>([]);
+  const [bonosSeleccionados, setBonosSeleccionados] = useState<{ [key: number]: number }>({});
+  const [apuestaSeleccionada, setApuestaSeleccionada] = useState<{ [key: number]: string | null }>({});
+  const [filteredFixtures, setFilteredFixtures] = useState<Fixture[]>([]);
+  const [selectedDate, setSelectedDate] = useState<string>('');
+  const [page, setPage] = useState(0);
+  const [paymentMethod, setPaymentMethod] = useState<'wallet' | 'webpay'>('webpay');
+  const fixturesPerPage = 10;
+  const { showModal } = useModal();
+
+  useEffect(() => {
+    const fetchFixtures = async () => {
+      try {
+        const response = await axios.get<Fixture[]>(`/fixtures/tradeable?page=${page}&count=${fixturesPerPage}`);
+        if (response.data.length === 0 && page > 0) {
+          setPage(page - 1)
+          return;
+        }
+        setFixtures(response.data);
+        setFilteredFixtures(response.data);
+      } catch (error) {
+        console.error('Error fetching matches:', error);
+      }
+    };
+    void fetchFixtures();
+  }, [page]);
+
+  // Filtrar por fecha
+  const handleDateFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selected = event.target.value;
+    setSelectedDate(selected);
+    if (selected) {
+      setFilteredFixtures(fixtures.filter(fixture => fixture.date.includes(selected)));
+    } else {
+      setFilteredFixtures(fixtures);
+    }
+  };
+
+  const findMatchWinnerOdd = (fixture: Fixture, team: string): OddValue | null => {
+    const odd = fixture.odds.find((odd) => odd.name === 'Match Winner');
+    return odd?.values.find((value) => value.bet === team) || null;
+  }
+
+  const handleComprar = async (id: number) => {
+    if (!user) {
+      await loginWithRedirect();
+      return;
+    }
+
+    const cantidadBonos = bonosSeleccionados[id] || 1;
+    const apuesta = apuestaSeleccionada[id];
+
+    const fixture: Fixture | undefined = fixtures.find(f => f.id === id);
+    if (!fixture) {
+      console.error('Fixture no encontrado.');
+      return;
+    }
+
+    let result: string;
+    if (apuesta === 'Local') {
+      result = fixture ? fixture.home_team.team.name : 'error';
+    } else if (apuesta === 'Visita') {
+      result = fixture ? fixture.away_team.team.name : 'error';
+    } else if (apuesta === 'Empate') {
+      result = '---';
+    } else {
+      console.error('Apuesta no seleccionada.');
+      return
+    }
+
+    const requestData: Offer = {
+      fixture_id: id,
+      result: result,
+      quantity: cantidadBonos,
+      uid: user.sub || 'error',
+    };
+
+    try {
+      await axios.post("/auctions/offer", requestData);
+      showModal('Oferta realizada con éxito', 'success');
+    } catch (error) {
+      console.error('Error realizando la oferta:', error);
+    }
+  };
+
+  const handleBonosChange = (id: number, value: number) => {
+    setBonosSeleccionados((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+  const handleApuestaChange = (id: number, apuesta: string) => {
+    setApuestaSeleccionada((prevState) => ({
+      ...prevState,
+      [id]: prevState[id] === apuesta ? null : apuesta,
+    }));
+  };
+
+  const handlePaymentMethodChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setPaymentMethod(event.target.value as 'webpay' | 'wallet');
+  };
+
+  const handleComprarWrapper = (id: number) => {
+    void handleComprar(id);
+  };
+
   return (
-    <div className="dashboard">
-      <Typography variant="h1" style={{ fontWeight: 700, color: "#B1CDEC" }}>
-        Dashboard
-      </Typography>
+    <div id="compras-container" style={{ color: "white" }}>
+      <h1 style={{ marginBottom: "50px" }}>Compra de Bonos</h1>
+      <div>
+        <label htmlFor="paymentMethod" style={{ marginRight: "20px", marginBottom: "50px" }}>Método de pago:</label>
+        <select id="paymentMethod" value={paymentMethod} onChange={handlePaymentMethodChange}>
+          <option value="webpay">Webpay</option>
+          <option value="wallet">Wallet</option>
+        </select>
+      </div>
+
+      <div>
+        <label htmlFor="dateFilter" style={{ marginRight: "20px", marginBottom: "50px" }}>Filtrar por fecha:</label>
+        <input
+          type="date"
+          id="dateFilter"
+          value={selectedDate}
+          onChange={handleDateFilter}
+        />
+      </div>
+
+      {filteredFixtures.length === 0 ? (
+        <p>No hay fixtures disponibles en este momento.</p>
+      ) : (
+        <div>
+          <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '20px' }}>
+            {fixtures.map((fixture) => (
+              <FixtureCard
+                key={fixture.id}
+                fixture={fixture}
+                apuestaSeleccionada={apuestaSeleccionada}
+                bonosSeleccionados={bonosSeleccionados}
+                handleApuestaChange={handleApuestaChange}
+                handleBonosChange={handleBonosChange}
+                handleComprar={handleComprarWrapper}
+                findMatchWinnerOdd={findMatchWinnerOdd}
+              />
+            ))}
+          </div>
+
+          <div>
+            {page > 0 && (
+              <button onClick={() => setPage(page - 1)}>Prev</button>
+            )}
+            <button onClick={() => setPage(page + 1)}>Next</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/views/admin/FixtureCard.tsx
@@ -0,0 +1,77 @@
+import { Fixture, OddValue } from '../../types/backend';
+
+export function FixtureCard(
+    { fixture, apuestaSeleccionada, bonosSeleccionados, handleApuestaChange, handleBonosChange, handleComprar, findMatchWinnerOdd }
+        : {
+            fixture: Fixture, apuestaSeleccionada: { [key: number]: string | null }, bonosSeleccionados: Record<number, number>, handleApuestaChange: (id: number, apuesta: string) => void, handleBonosChange: (id: number, bonos: number) => void, handleComprar: (id: number) => void,
+            findMatchWinnerOdd: (fixture: Fixture, team: string) => OddValue | null
+        }
+) {
+
+    const apuestaSeleccionadaToQuantity = (apuesta: string | null) => {
+        if (apuesta === null) {
+            return 0;
+        }
+        if (apuesta === 'Local') {
+            return fixture.reserved_home;
+        } else if (apuesta === 'Empate') {
+            return fixture.reserved_draw;
+        } else if (apuesta === 'Visita') {
+            return fixture.reserved_away;
+        }
+        return 0;
+    }
+    return (
+        <div key={fixture.id} className="compra-item">
+            <p><strong>Liga:</strong> {fixture.league.name}</p>
+            <p><strong>Local:</strong> {fixture.home_team.team.name}</p>
+            <p><strong>Visita:</strong> {fixture.away_team.team.name}</p>
+            <p><strong>Referee:</strong> {fixture.referee}</p>
+            <p><strong>Fecha:</strong> {fixture.date}</p>
+
+            <div className="apuestas-container">
+                <div className="apuesta-item">
+                    <p><strong>Gana {fixture.home_team.team.name}:</strong> {findMatchWinnerOdd(fixture, 'Home')?.value}</p>
+                    <button
+                        className={apuestaSeleccionada[fixture.id] === 'Local' ? 'selected' : ''}
+                        onClick={() => handleApuestaChange(fixture.id, 'Local')}
+                    >
+                        Gana Local ({fixture.reserved_home})
+                    </button>
+                </div>
+                <div className="apuesta-item">
+                    <p><strong>Empate:</strong> {findMatchWinnerOdd(fixture, 'Draw')?.value}</p>
+                    <button
+                        className={apuestaSeleccionada[fixture.id] === 'Empate' ? 'selected' : ''}
+                        onClick={() => handleApuestaChange(fixture.id, 'Empate')}
+                    >
+                        Empate ({fixture.reserved_draw})
+                    </button>
+                </div>
+                <div className="apuesta-item">
+                    <p><strong>Gana {fixture.away_team.team.name}:</strong> {findMatchWinnerOdd(fixture, 'Away')?.value}</p>
+                    <button
+                        className={apuestaSeleccionada[fixture.id] === 'Visita' ? 'selected' : ''}
+                        onClick={() => handleApuestaChange(fixture.id, 'Visita')}
+                    >
+                        Gana Visita ({fixture.reserved_away})
+                    </button>
+                </div>
+            </div>
+
+            <label htmlFor={`bonos-${fixture.id}`}>Selecciona cantidad de bonos (1 a {fixture.remaining_bets}):</label>
+            <input
+                className='comprar-input'
+                type="number"
+                id={`bonos-${fixture.id}`}
+                name="bonos"
+                min="1"
+                max={apuestaSeleccionadaToQuantity(apuestaSeleccionada[fixture.id])}
+                value={bonosSeleccionados[fixture.id] || 1}
+                onChange={(e) => handleBonosChange(fixture.id, parseInt(e.target.value))}
+            />
+
+            <button className='comprar-button' onClick={() => void handleComprar(fixture.id)}>Comprar</button>
+        </div>
+    )
+}
